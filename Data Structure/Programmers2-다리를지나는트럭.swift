1e951428762e17fb287097af24f18e6eaa19091b
yurim <bigtoy2645@naver.com>
2021-06-09 01:56:32 +0900

    feat: 프로그래머스2 다리를 지나는 트럭 솔루션 수정

--- Data Structure/Programmers2-다리를지나는트럭.swift
+++ Data Structure/Programmers2-다리를지나는트럭.swift
@@ -21,33 +21,37 @@
 
  solution 함수의 매개변수로 다리에 올라갈 수 있는 트럭 수 bridge_length, 다리가 견딜 수 있는 무게 weight, 트럭 별 무게 truck_weights가 주어집니다.
  이때 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지 return 하도록 solution 함수를 완성하세요.
-
- 제한 조건)
- bridge_length는 1 이상 10,000 이하입니다.
- weight는 1 이상 10,000 이하입니다.
- truck_weights의 길이는 1 이상 10,000 이하입니다.
- 모든 트럭의 무게는 1 이상 weight 이하입니다.
  */
 
 import Foundation
 
 class Truck {
     func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
-        var queue = [truck_weights[0]]
-        var time = 1, i = 1
+        var bridge = [Int]()
+        var current_weights = 0
+        var time = 0, i = 0, count = 0
         
-        while queue.count > 0, i < truck_weights.count {
+        while count < truck_weights.count { // 다리를 지난 트럭 개수 비교
             time += 1
-            queue.removeFirst()
             
-            if queue.count < bridge_length,
-               queue.reduce(0, +) + truck_weights[i] <= weight {
-                queue.append(truck_weights[i])
+            // 큐에서 다리를 지난 트럭 제거
+            if bridge.count == bridge_length {
+                let removeWeight = bridge.removeFirst()
+                if removeWeight > 0 {
+                    current_weights -= removeWeight
+                    count += 1
+                }
+            }
+            // 다리를 건널 수 있으면 큐에 추가
+            if i < truck_weights.count, current_weights + truck_weights[i] <= weight {
+                bridge.append(truck_weights[i])
+                current_weights += truck_weights[i]
                 i += 1
+            // 다리를 건널 수 없으면 0 추가
+            } else {
+                bridge.append(0)
             }
         }
-        
-        print(time)
         
         return time
     }
