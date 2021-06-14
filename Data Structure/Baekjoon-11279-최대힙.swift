3b3b8ab93ee3edbcff2c3a90c4a557a95ea321ba
yurim <bigtoy2645@naver.com>
2021-06-15 01:01:44 +0900

    feat: 백준 11279 솔루션 수정

--- Data Structure/Baekjoon-11279-최대힙.swift
+++ Data Structure/Baekjoon-11279-최대힙.swift
@@ -10,9 +10,34 @@
  배열에서 가장 큰 값을 출력하고, 그 값을 배열에서 제거한다.
  프로그램은 처음에 비어있는 배열에서 시작하게 된다.
 
- 입력) 첫째 줄에 연산의 개수 N(1 ≤ N ≤ 100,000)이 주어진다. 다음 N개의 줄에는 연산에 대한 정보를 나타내는 정수 x가 주어진다. 만약 x가 자연수라면 배열에 x라는 값을 넣는(추가하는) 연산이고, x가 0이라면 배열에서 가장 큰 값을 출력하고 그 값을 배열에서 제거하는 경우이다. 입력되는 자연수는 231보다 작다.
+ 입력) 첫째 줄에 연산의 개수 N(1 ≤ N ≤ 100,000)이 주어진다. 다음 N개의 줄에는 연산에 대한 정보를 나타내는 정수 x가 주어진다. 만약 x가 자연수라면 배열에 x라는 값을 넣는(추가하는) 연산이고, x가 0이라면 배열에서 가장 큰 값을 출력하고 그 값을 배열에서 제거하는 경우이다. 입력되는 자연수는 2^31보다 작다.
 
  출력) 입력에서 0이 주어진 회수만큼 답을 출력한다. 만약 배열이 비어 있는 경우인데 가장 큰 값을 출력하라고 한 경우에는 0을 출력하면 된다.
+ 
+ 예시)
+13
+0
+1
+2
+0
+0
+3
+2
+1
+0
+0
+0
+0
+0
+->
+0
+2
+1
+3
+2
+1
+0
+0
  */
 
 func maxHeap() {
@@ -22,26 +47,35 @@ func maxHeap() {
     for _ in 0..<N {
         let x = Int(readLine()!)!
         if x == 0 {
-            arr.sort()
-            print(arr.popLast() ?? 0)
+            if arr.count == 0 { print(0) }
+            else {
+                var max = 0, maxIndex = 0
+                for i in 0..<arr.count {
+                    if max < arr[i] {
+                        max = arr[i]
+                        maxIndex = i
+                    }
+                }
+                arr[maxIndex] = 0
+                print(max)
+            }
         } else {
             arr.append(x)
         }
     }
-//    
-//    for _ in 0..<N {
-//        let x = Int(readLine()!)!
-//        if x == 0 {
-//            print(arr.popLast() ?? 0)
-//        } else {
-//            var middle = false
-//            for i in 0..<arr.count {
-//                if x < arr[i] {
-//                    arr.insert(x, at: i)
-//                    middle = true
-//                }
-//            }
-//            if middle == false { arr.append(x) }
-//        }
-//    }
 }
+
+/* 시간초과
+ let N = Int(readLine()!)!
+ var arr = [Int]()
+
+ for _ in 0..<N {
+     let x = Int(readLine()!)!
+     if x == 0 {
+         arr.sort()
+         print(arr.popLast() ?? 0)
+     } else {
+         arr.append(x)
+     }
+ }
+ */
